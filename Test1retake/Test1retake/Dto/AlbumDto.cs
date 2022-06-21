cf9a52abb5424c7707665a744b0a56aeaa40aab8
s20824 <s20824@pjwstk.edu.pl>
2022-06-21 08:59:57 +0200

    Add AlbumDto

---
+++ Test1retake/Test1retake/Dto/AlbumDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Test1retake.Dto
+{
+    public class AlbumDto
+    {
+        public int IdAlbum { get; set; }
+        public string AlbumName { get; set; }
+        public DateTime PublishDate { get; set; }
+        public int IdMusicLabel { get; set; }
+        public int IdTrack { get; set; }
+        public string TrackName { get; set; }
+        public float Duration { get; set; }
+    }
+}
